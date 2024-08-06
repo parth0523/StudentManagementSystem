00b4a40b03d664d6aaecd6e5f222c571bf98b841
Ayush Kumar Sinha <ayushsiha085@gmail.com>
2024-08-06 13:19:11 +0530

    a class faculty was created

---
+++ StudentManagementSystem/Faculty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+     public class Faculty
+    {
+        public int facultyId { get; set; }
+        public string facultyName { get; set; }
+        public string facultyDescription { get; set; }
+        public decimal facultySalary { get; set; }
+
+    }
+}
