4a5fbeb94985303028ab088bcd97eae90e4ed979
Parth Naseri <parth-rishikesh.naseri@capgemini.com>
2024-08-06 13:01:06 +0530

    This is Parth Respository

---
+++ StudentManagementSystem/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+    internal class Course
+    {
+        public int CourseID { get; set; }
+        public int CourseName { get; set; }   
+
+        public int StudentsEnrolled { get; set; }   // No of students enrolled in the specific course 
+        public int CoursePrice { get; set; }
+
+    }
+}
